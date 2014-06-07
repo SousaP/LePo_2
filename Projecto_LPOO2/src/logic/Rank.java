db59d2299a966614083b27dff8fea5da710d2619
Mouzician <mfilipe_13@hotmail.com>
2014-06-07 20:12:48 +0100

    jdkhajdk

--- Projecto_LPOO2/src/logic/Rank.java
+++ Projecto_LPOO2/src/logic/Rank.java
@@ -1,18 +1,27 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
+public class Rank {
 
-public class Rank{
+	private static Comparator<Player> COMPARATOR = new Comparator<Player>() {
+		// This is where the sorting happens.
+		public int compare(Player p1, Player p2) {
+			return p1.getScore() - p2.getScore();
+		}
+	};
 
-	Player Top10[];
-	
-	public Rank(){
-		
+	List<Player> products = new ArrayList<Player>(10);
+
+	public Rank() {
+
 	}
-	
-	public String[] getRank(){
+
+	public String[] getRank() {
 		return null;
-		
+
 	}
 }
